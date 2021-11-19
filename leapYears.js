41adc7366f414c6bd4011131bd0bac2785448e8e
lamalajaji <Lama.Mohammed.elq.js@tuwaiq.edu.sa>
2021-11-19 14:07:46 +0300

    leap years solving

---
+++ leapYears.js
@@ -0,0 +1,13 @@
+function isLeapYear(year) {
+  // TODO
+  if (year % 4 === 0) {
+    if (year % 400 === 0 && year % 100 !== 0) {
+    }
+    return true;
+  } else {
+    return false;
+  }
+}
+
+
+///// actully i thought this problem will be complicated but i solve it in first test :)
